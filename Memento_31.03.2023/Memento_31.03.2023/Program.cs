a0fdac6fc0a6ee8dbe66fa64fe4a8eba1659f7cc
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-03 22:57:32 +0300

    hw

--- Memento_31.03.2023/Memento_31.03.2023/Program.cs
+++ Memento_31.03.2023/Memento_31.03.2023/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StateNS
+namespace Memento
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
         }
     }
 }
--- Stratagy_03.04.2023/Stratagy_03.04.2023/Program.cs
+++ Stratagy_03.04.2023/Stratagy_03.04.2023/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StateNS
+namespace Stratagy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
         }
     }
 }
