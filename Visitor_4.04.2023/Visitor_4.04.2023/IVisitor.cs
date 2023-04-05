6dad57f4f30ba8ad2d6f1e113b02c97bb87f70bf
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-04-05 21:29:14 +0300

    Visitor

---
+++ Visitor_4.04.2023/Visitor_4.04.2023/IVisitor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisitorNS
+{
+    public interface IComponent
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class HouseComponent : IComponent
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitHouseComponent(this);
+        }
+
+        public string ExclusiveMethodOfHouseComponent()
+        {
+            return "House Component";
+        }
+    }
+
+    public class BankComponent : IComponent
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitConcreteComponentB(this);
+        }
+
+        public string SpecialMethodOfConcreteComponentB()
+        {
+            return "BankComponent";
+        }
+    }
+    public interface IVisitor
+    {
+        void VisitHouseComponent(HouseComponent element);
+
+        void VisitConcreteComponentB(BankComponent element);
+    }
+    class ConcreteVisitor1 : IVisitor
+    {
+        public void VisitHouseComponent(HouseComponent element)
+        {
+            Console.WriteLine(element.ExclusiveMethodOfHouseComponent() + " + ConcreteVisitor1");
+        }
+
+        public void VisitConcreteComponentB(BankComponent element)
+        {
+            Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor1");
+        }
+    }
+
+    class ConcreteVisitor2 : IVisitor
+    {
+        public void VisitHouseComponent(HouseComponent element)
+        {
+            Console.WriteLine(element.ExclusiveMethodOfHouseComponent() + " + ConcreteVisitor2");
+        }
+
+        public void VisitConcreteComponentB(BankComponent element)
+        {
+            Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor2");
+        }
+    }
+
+    public class Client
+    {
+        public static void ClientCode(List<IComponent> components, IVisitor visitor)
+        {
+            foreach (var component in components)
+            {
+                component.Accept(visitor);
+            }
+        }
+    }
+
+   
+}
---
+++ Visitor_4.04.2023/Visitor_4.04.2023/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IComponent> components = new List<IComponent>
+            {
+                new HouseComponent(),
+                new HouseComponent()
+            };
+
+            Console.WriteLine("The client code works with all visitors via the base Visitor interface:");
+            var visitor1 = new ConcreteVisitor1();
+            Client.ClientCode(components, visitor1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("It allows the same client code to work with different types of visitors:");
+            var visitor2 = new ConcreteVisitor2();
+            Client.ClientCode(components, visitor2);
+        }
+    }
+}
